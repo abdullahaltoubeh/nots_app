24932049320893e1078c835cac1200588015566c
Abdullah <abdullahaltobeh19@gmail.com>
2024-07-25 16:32:48 +0300

    crate addNote to add note cubit

--- lib/cubits/add_note_cubit/add_note_cubit.dart
+++ lib/cubits/add_note_cubit/add_note_cubit.dart
@@ -1,11 +1,22 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hive_flutter/adapters.dart';
+import 'package:nots_app/consts_file.dart';
 import 'package:nots_app/models/note_model.dart';
 
+import '../../models/note_model.dart';
 import'add_note_states.dart';
 class NoteCubit extends Cubit<AddNoteStates> {
   NoteCubit() : super (AddNoteInitial());
 
-  addNote(NoteModel){
+  addNote(NoteModel note) async {
+    emit(AddNoteLoading());
+    try {
+      var noteBox = Hive.box<NoteModel>(kNoteBox);
+      emit(AddNoteSuccess());
+     await noteBox.add(note);
+    } catch (e) {
+      AddNoteFailure(e.toString());
 
+    }
   }
 }
--- lib/cubits/add_note_cubit/add_note_states.dart
+++ lib/cubits/add_note_cubit/add_note_states.dart
@@ -5,4 +5,7 @@ abstract class AddNoteStates{}
 class AddNoteInitial extends AddNoteStates{}
 class AddNoteLoading extends AddNoteStates{}
 class AddNoteSuccess extends AddNoteStates{}
-class AddNoteFailure extends AddNoteStates{}
+class AddNoteFailure extends AddNoteStates{
+  final errMessage;
+  AddNoteFailure(this.errMessage );
+}
